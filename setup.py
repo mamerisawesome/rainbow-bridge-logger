fd5ff9177c6e56e7df799831c316ebc628ca9472
mamerisawesome <atmalmer23@gmail.com>
2018-10-11 14:20:16 +0800

    Addedd image and updated setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description='Improved logger based on Python\'s logging module',
     packages=['rainbow',],
     license='MIT',
-    long_description=open('README.txt').read(),
+    long_description=open('long-description.txt').read(),
     author='Almer Mendoza',
     author_email='atmalmer23@gmail.com'
 )
