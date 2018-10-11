c87eb9c2d44e840cabbfe689b3dc67083e53f055
mamerisawesome <atmalmer23@gmail.com>
2018-10-11 17:02:19 +0800

    Updated for publish

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ setup(
     description='Improved logger based on Python\'s logging module',
     packages=['rainbow',],
     license='MIT',
-    long_description=open('long-description.txt').read(),
+    download_url='https://github.com/mamerisawesome/rainbow-bridge-logger/archive/v_01.tar.gz',
+    long_description=open('README.md').read(),
     author='Almer Mendoza',
     author_email='atmalmer23@gmail.com'
 )
