7db9e13258df1a531695f35adcf9a16def1190ff
mamerisawesome <atmalmer23@gmail.com>
2018-10-12 02:49:11 +0800

    Updated readme for publish

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
     name='rainbow-bridge-logger',
-    version='0.2',
+    version='0.3',
     description='Improved logger based on Python\'s logging module',
     packages=['rainbow',],
     license='MIT',
     url = 'https://github.com/mamerisawesome/rainbow-bridge-logger',
-    download_url='https://github.com/mamerisawesome/rainbow-bridge-logger/archive/v_0.2.tar.gz',
-    long_description=open('long-description.txt').read(),
+    download_url='https://github.com/mamerisawesome/rainbow-bridge-logger/archive/v_0.3.tar.gz',
+    long_description=open('long-description.rst').read(),
     author='Almer Mendoza',
     author_email='atmalmer23@gmail.com'
 )
