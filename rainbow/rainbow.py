c7138077d4d3bd7c2840ec76ff31026cfdbfb702
Almer Mendoza <atmalmer23@gmail.com>
2019-03-15 11:45:45 +0800

    Added global handling of logging module

--- rainbow/rainbow.py
+++ rainbow/rainbow.py
@@ -2,12 +2,14 @@
 :Author: Almer Mendoza
 :Since: 10/10/2018
 """
-import logging
-
 _logging_module = None
+_is_no_color = False
 
 def _get_color(color=None):
   """Get color name from pre-defined color list"""
+  if _is_no_color:
+    return ''
+
   if color is None and not color:
     raise ValueError('Must have color code')
 
@@ -28,19 +30,20 @@ def _get_color(color=None):
 
   return colors[color]
 
-def _get_message():
+def _get_message(no_time=False):
   """Get message parts"""
-  return [
-    '{}%(asctime)s{}'.format(_get_color('DARKGRAY'), _get_color('GREY')),
-    '{}%(name)-12s{}'.format(_get_color('PURPLE'), _get_color('GREY')),
-    '%(levelname)-8s',
-    '%(message)s'
-  ]
+  time = '{}%(asctime)s{}'.format(_get_color('DARKGRAY'), _get_color('GREY'))
+  name = '{}%(name)-12s{}'.format(_get_color('PURPLE'), _get_color('GREY'))
+  level = '%(levelname)-8s'
+  message = '%(message)s'
+
+  if no_time:
+    return '{} {}\t{}'.format(name, level, message)  
+
+  return '{} {} {}\t{}'.format(time, name, level, message)
 
 def _set_level_format(level=None, color='WHITE'):
   """Set logging format based on level and color"""
-  global _logging_module
-
   FORMAT = '{}{}{}'
   parsed_format = FORMAT.format(
     _get_color(color),
@@ -48,45 +51,55 @@ def _set_level_format(level=None, color='WHITE'):
     _get_color('GREY')
   )
 
-  if _logging_module.DEBUG is not None:
-    parsed_level = _logging_module.DEBUG
-
   if level is not None:
-    parsed_level = level
-
-  if parsed_level is not None:
-    _logging_module.addLevelName(parsed_level, parsed_format)
+    _logging_module.addLevelName(level, parsed_format)
 
   return True
 
-def RainbowLogger(name=None, no_time=False, new_logging=None):
+def RainbowLogger(name=None, no_time=False, no_color=False, new_logging=None, filepath=None, log_level=None, get_logging=False):
   """A customized logger built on top of Python's logging"""
+  import logging  
   global _logging_module
+  global _is_no_color
 
+  _is_no_color = no_color
   _logging_module = logging
   if new_logging is not None:
     _logging_module = new_logging
 
   _set_level_format(_logging_module.DEBUG, 'BLUE')
   _set_level_format(_logging_module.INFO, 'GREEN')
-  _set_level_format(_logging_module.WARNING, 'YELLOW')
   _set_level_format(_logging_module.WARN, 'YELLOW')
   _set_level_format(_logging_module.ERROR, 'RED')
   _set_level_format(_logging_module.CRITICAL, 'MAGENTA')
 
-  logger = _logging_module.getLogger(__name__ if name is None else name)
+  if name is None:
+    logger = _logging_module
+  else:
+    logger = _logging_module.getLogger(name)
+
   handler = _logging_module.StreamHandler()
-  (time, name, level, message) = _get_message()
-  final_message = '{} {} {}\t{}'.format(time, name, level, message)
-
-  if no_time:
-    final_message = '{} {}\t{}'.format(name, level, message)
-
+  final_message = _get_message(no_time)
   formatter = _logging_module.Formatter(final_message)
 
   handler.setFormatter(formatter)
-  logger.addHandler(handler)
-  logger.setLevel(_logging_module.DEBUG)
+  if name is None:
+    _logging_module.basicConfig(format=final_message,
+                                level=log_level if log_level else _logging_module.DEBUG)
+  else:
+    logger.addHandler(handler)
+    logger.setLevel(log_level if log_level else _logging_module.DEBUG)
+
+  if filepath and name:
+    handler = _logging_module.FileHandler(filepath)
+    _is_no_color = True
+    final_message = _get_message(no_time)
+    formatter = _logging_module.Formatter(final_message)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+  if get_logging:
+    return _logging_module
 
   return logger
 
