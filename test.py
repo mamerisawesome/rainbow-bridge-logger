e71cfa4a78708c2b7bed84155f67fac7431c3d5b
mamerisawesome <atmalmer23@gmail.com>
2020-04-03 12:34:37 +0800

    Added root test cases; may need to see pytest on version error

--- test.py
+++ test.py
@@ -1,8 +1,18 @@
 from rainbow import RainbowLogger
 
-if __name__ == '__main__':
-  logger = RainbowLogger(__name__)
-  logger.info('my info')
-  logger.warning('my warn')
-  logger.error('my error')
-  logger.debug('my debug')
+if __name__ == "__main__":
+  # root logger
+  logger = RainbowLogger()
+  logger.info("root info")
+  logger.warning("root warn")
+  logger.error("root error")
+  logger.debug("root debug")
+  logger.critical("root critical")
+
+  logger = RainbowLogger("rainbow")
+  logger.info("my info")
+  logger.warning("my warn")
+  logger.error("my error")
+  logger.debug("my debug")
+  logger.critical("my critical")
+
